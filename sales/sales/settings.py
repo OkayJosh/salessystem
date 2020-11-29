2942a064301c8215ce613fc496c5758130806200
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-11-29 23:01:33 +0100

    Update settings.py

--- sales/sales/settings.py
+++ sales/sales/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'sales/managesales',
+    'managesales',
 
     'rest_framework',
     'phonenumber_field',
