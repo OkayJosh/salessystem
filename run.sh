9c2d45e5694eae0ef1253a291183dbb71a3c185f
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-11-29 22:42:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 python sales/manage.py migrate
-gunicorn sales/sales.wsgi:application --bind 0.0.0.0:8000 --log-level info --timeout 180  --workers 3
+gunicorn sales.sales.wsgi:application --bind 0.0.0.0:8000 --log-level info --timeout 180  --workers 3
